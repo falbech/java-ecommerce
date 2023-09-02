4777ac641d50a10ac5b7bc0e6f06217676fd4812
Felipe Martinez Albeche <felipealbeche@hotmail.com>
2023-09-02 13:34:34 -0300

    refactor common dependencies

--- common-kafka/src/main/java/br/com/alura/ecommerce/KafkaService.java
+++ common-kafka/src/main/java/br/com/alura/ecommerce/KafkaService.java
@@ -49,7 +49,7 @@ public class KafkaService<T> implements Closeable {
 		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
 		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GsonDeserializer.class.getName());
-//		properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
+		properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
 		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
 		properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
 		properties.setProperty(GsonDeserializer.TYPE_CONFIG, type.getName());
