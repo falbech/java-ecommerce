495aca5ad73f63561a384ec9bbd55e08175eebe6
Felipe Martinez Albeche <felipealbeche@hotmail.com>
2023-09-03 08:44:13 -0300

    add create-user-service

---
+++ service-users/src/main/java/br/com/jstore/ecommerce/Order.java
@@ -0,0 +1,33 @@
+package br.com.jstore.ecommerce;
+
+import java.math.BigDecimal;
+
+public class Order {
+
+	private final String userId, orderId;
+	private final BigDecimal amount;
+	
+	public Order(String userId, String orderId, BigDecimal amount) {
+		this.userId = userId;
+		this.orderId = orderId;
+		this.amount = amount;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public String getEmail() {
+		return "";
+	}
+	
+	
+}
